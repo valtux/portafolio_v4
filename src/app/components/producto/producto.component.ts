29be8e8515588c17ea3d0fb000018099924c4eee
tuxval <valeriashanahan@gmail.com>
2018-02-07 18:30:59 -0300

    bk

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {ProductosService} from "../../services/productos.service";
 
 @Component({
   selector: 'app-producto',
@@ -7,12 +8,20 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ProductoComponent  {
 
-  constructor(private route: ActivatedRoute) {
+  producto:any = undefined;
+
+  constructor(private route: ActivatedRoute,
+              private _ps:ProductosService) {
 
       route.params.subscribe(parametros => {
 
-        console.log(parametros);
+        // console.log(parametros);
         console.log(parametros['id']);
+        _ps.cargar_producto(parametros['id'])
+              .subscribe(res => {
+                console.log(res.json());
+                this.producto = res.json();
+              })
       })
   }
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -13,6 +13,11 @@ export class ProductosService {
     this.cargar_productos();
   }
 
+  public cargar_producto(cod:string){
+
+    return this.http.get(`https://paginaweb-d7ee5.firebaseio.com/productos/${ cod }.json`);
+  }
+
   public cargar_productos(){
       this.cargando=true;
 
